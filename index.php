847b4804dc5d4f21794eff304efee64d2e187b0b
Arcadiumstreet <brochud.c@gmail.com>
2023-04-26 18:33:35 +0200

    structure

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Agenda</title>
-    <link rel="stylesheet" href="styles/agenda.css" type="text/css" />
-  </head>
-  <body>
-    <main>
-      <header>
-        <h1>Agenda de la semaine</h1>
-      </header>
-    </main>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="styles/Randonnée.css"/>
+  <title>Page d'accueil</title>
+</head>
+<body>
+  <div id="wrapper">
+    <header>
+      <h1>Parcours de randonnée</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Randonnées</a></li>
+        <li><a href="#">se connecter</a></li>
+      </ul>
+    </nav> 
+    <section>
+      <h2>Bienvenue sur notre site de randonnée</h2>
+      <p>Recherchez et trouvez les meilleurs parcours de randonnée dans votre région !</p>
+      <form>
+        
+      </form>
+    </section>
+  </div>
+</body>
 </html>
