7ed1181091387325ce58581995956e2f777e6f50
theophane16 <theophane.paradis007@gmail.com>
2023-04-28 01:21:49 +0200

    Acceuil_Sympa

--- index.php
+++ index.php
@@ -11,14 +11,29 @@
       <div class="logo">
         <a href="index.php"><span class="R">R</span><span>d</span><span class="F">F</span></a>
       </div>
+      <nav>
+        <ul>
+          <li class="active"><a href="Index.php">Accueil</a></li>
+          <li><a href="Randonne.php">Randonnée</a></li>
+          <li><a href="Contribuer.php">contribuer</a></li>
+          <li><a href="Connexion.php">Connexion</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="titre">
+      <h1> Besoin d'une randonnée ?</h1>
+    </div>
+    
+    <div class="button">
+    <a href="./photos/Sujet_Projet.pdf" download="Sujet_du_Projet.pdf" class="bouton-telecharger">Télécharger le sujet </a>
     </div>
   </header>
 
 
-
+<!--
   <div id="wrapper">
     <header>
-      <h1>Parcours de randonnée</h1>
+      <h1 class="active">Parcours de randonnée</h1>
       <nav>
         <ul>
           <li><a href="Index.php">Accueil</a></li>
@@ -37,5 +52,7 @@
   </div>
 
   <a href="#haut">haut de page</a>
+
+-->
 </body>
 </html>
