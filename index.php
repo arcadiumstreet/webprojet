d0d469e119bfcbbdd5ba3353eef475de311b3328
theophane16 <theophane.paradis007@gmail.com>
2023-04-27 23:17:47 +0200

    debutCSS

--- index.php
+++ index.php
@@ -2,28 +2,40 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8"/>
-  <link rel="stylesheet" href="styles/Randonnée.css"/>
+  <link rel="stylesheet" href="styles/index.css"/>
   <title>Page d'accueil</title>
 </head>
 <body>
+  <header>
+    <div class="principale">
+      <div class="logo">
+        <a href="index.php"><span class="R">R</span><span>d</span><span class="F">F</span></a>
+      </div>
+    </div>
+  </header>
+
+
+
   <div id="wrapper">
     <header>
       <h1>Parcours de randonnée</h1>
+      <nav>
+        <ul>
+          <li><a href="Index.php">Accueil</a></li>
+          <li><a href="Randonne.php">Randonnée</a></li>
+          <li><a href="Contribuer.php">contribuer</a></li>
+          <li><a href="Connexion.php">Connexion</a></li>
+        </ul>
+      </nav>
     </header>
-    <nav>
-      <ul>
-        <li><a href="Index.php">Accueil</a></li>
-        <li><a href="Randonne.php">Randonnée</a></li>
-        <li><a href="Contribuer.php">contribuer</a></li>
-        <li><a href="Connexion.php">Connexion</a></li>
-      </ul>
-    </nav> 
+     
     <section>
       <h2>Bienvenue sur notre site de randonnée</h2>
       <p>Recherchez et trouvez les meilleurs parcours de randonnée dans votre région !</p>
      
     </section>
   </div>
+
   <a href="#haut">haut de page</a>
 </body>
 </html>
